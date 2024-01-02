6f71600d75cbc4b4ef6a7ec93ec5f07a12ee166a
ceramic-vessel-developer <263973@student.pwr.edu.pl>
2024-01-02 21:00:46 +0100

    lab4 4.0

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -6,15 +6,22 @@ from glfw.GLFW import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
 
+import math
+
 
 viewer = [0.0, 0.0, 10.0]
 
 theta = 0.0
+phi = 0.0
 pix2angle = 1.0
+R = 10
 
 left_mouse_button_pressed = 0
 mouse_x_pos_old = 0
 delta_x = 0
+
+mouse_y_pos_old = 0
+delta_y = 0
 
 mat_ambient = [1.0, 1.0, 1.0, 1.0]
 mat_diffuse = [1.0, 1.0, 1.0, 1.0]
@@ -88,7 +95,7 @@ def shutdown():
 
 
 def render(time):
-    global theta
+    global theta, phi, light_position
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glLoadIdentity()
@@ -98,14 +105,32 @@ def render(time):
 
     if left_mouse_button_pressed:
         theta += delta_x * pix2angle
+        phi += delta_y * pix2angle
 
-    glRotatef(theta, 0.0, 1.0, 0.0)
+    # glRotatef(theta, 0.0, 1.0, 0.0)
+
+    theta %= 360
+    phi %= 360
+
+    light_position[0] = R * math.cos(theta * math.pi / 180) * math.cos(phi * math.pi / 180)
+    light_position[1] = R * math.sin(phi * math.pi / 180)
+    light_position[2] = R * math.sin(theta * math.pi / 180) * math.cos(phi * math.pi / 180)
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position)
 
     quadric = gluNewQuadric()
     gluQuadricDrawStyle(quadric, GLU_FILL)
     gluSphere(quadric, 3.0, 10, 10)
     gluDeleteQuadric(quadric)
-    # lights()
+
+    # light visualization
+    glTranslatef(light_position[0], light_position[1], light_position[2])
+    quadric = gluNewQuadric()
+    gluQuadricDrawStyle(quadric, GLU_LINE)
+    gluSphere(quadric, 0.5, 6, 5)
+    gluDeleteQuadric(quadric)
+    glTranslatef(-light_position[0], -light_position[1], -light_position[2])
+
+
 
     glFlush()
 
@@ -164,7 +189,6 @@ def increase_colour(step):
     lights()
 
 
-
 def decrease_colour(step):
     global light_ambient, light_diffuse, light_specular
     if choice[1] == 0:
@@ -182,7 +206,6 @@ def decrease_colour(step):
     lights()
 
 
-
 def print_choice():
     if choice[1] == 0:
         print(f"Changing ambient, argument num{choice[0]}")
@@ -193,8 +216,11 @@ def print_choice():
 
 
 def mouse_motion_callback(window, x_pos, y_pos):
-    global delta_x
-    global mouse_x_pos_old
+    global delta_x, delta_y
+    global mouse_x_pos_old, mouse_y_pos_old
+
+    delta_y = y_pos - mouse_y_pos_old
+    mouse_y_pos_old = y_pos
 
     delta_x = x_pos - mouse_x_pos_old
     mouse_x_pos_old = x_pos
